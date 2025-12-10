03a66b403ffa4f63c0d37e20aee709c832a8ff58
Felipe Cruz <fcruzf01@liverpool.com.mx>
2025-12-10 09:10:45 -0600

    remove code from main

--- src/main/java/com/kartikay/sales_order_management_api/dto/AuthResponse.java
+++
@@ -1,3 +0,0 @@
-package com.kartikay.sales_order_management_api.dto;
-
-public record AuthResponse(String token) {}
--- src/main/java/com/kartikay/sales_order_management_api/dto/CatalogDTO.java
+++
@@ -1,9 +0,0 @@
-package com.kartikay.sales_order_management_api.dto;
-
-import java.math.BigDecimal;
-
-public record CatalogDTO(
-        Long id,
-        String name,
-        BigDecimal price
-) {}
